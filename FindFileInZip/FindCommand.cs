1441c754ff8f000d61b3029cf0cb46b5abda8ec1
Kenya Matsumoto <kmatsumoto@mdsol.com>
2015-04-10 15:20:00 +0900

    initial commit.

---
+++ FindFileInZip/FindCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Ionic.Zip;
+using ManyConsole;
+
+namespace FindFileInZip
+{
+    public class FindCommand : ConsoleCommand
+    {
+        private string dictionaryPath; // Dictionary directory path or zip file path.
+        private string pathInZip; // Search path in zip file.
+
+        public FindCommand()
+        {
+            IsCommand("find", "find file in zip.");
+            HasRequiredOption("d|dictionaryPath=", "The dictionary directory path or zip file path", v => dictionaryPath = v);
+            HasRequiredOption("p|pathInZip=", "The search path in zip file", v => pathInZip = v);
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            var zipFiles = new DirectoryInfo(dictionaryPath).GetFiles("*.zip", SearchOption.TopDirectoryOnly);
+
+            pathInZip = WebUtility.UrlDecode(pathInZip);
+
+            var found = false;
+            foreach (var zipFile in zipFiles)
+            {
+                using (var zip = ZipFile.Read(zipFile.FullName))
+                {
+                    foreach (var zipEntry in zip.Entries)
+                    {
+                        if (zipEntry.IsDirectory)
+                        {
+                            continue;
+                        }
+
+                        var path = zipEntry.FileName.Replace('\\', '/');
+                        // Console.WriteLine(path);
+                        if (path == pathInZip)
+                        {
+                            found = true;
+                            Console.WriteLine("{0} in {1}", pathInZip, zipFile.FullName);
+                            break;
+                        }
+
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("{0} is not found.", pathInZip);
+            }
+
+            Console.WriteLine("Please press some keys.");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # FindFileInZip
+
+Find file in all zip files. 
+
+### Command Usage
+
+FindFileInZip.exe **find** [OPTIONS]
+
+- `dictionaryPath` : Target directory path that contains zip files. ex.) `C:\zip_files`
+- `pathInZip` : Search path you want to find. ex.) `dist/css/bootstrap-theme.css`
+
+```
+FindFileInZip.exe find --dictionaryPath=C:\zip_files --pathInZip=dist/css/bootstrap-theme.css
+```
